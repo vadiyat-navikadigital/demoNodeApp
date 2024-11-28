d4fa45ac71a2b4a43cc81f0b38389a825ac36837
vadiyat-navikadigital <vadiyat.naqvi@navikadigital.com>
2024-11-28 19:05:40 +0530

    files deleted

--- routes/queryExportCsvRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const queryController = require("../controllers/queryController");
-
-router.get("/export-csv", queryController.exportCsv);
-
-module.exports = router;
